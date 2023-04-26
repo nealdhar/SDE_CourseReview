28a56fa0ecdd383cb07cc11660b78a3774124fa5
Olivia Fountain <123495218+ogf9uhy@users.noreply.github.com>
2023-04-26 18:11:32 -0400

    Implementation class for DatabaseManager

---
+++ src/main/java/data/DataBaseManagerImpl.java
@@ -0,0 +1,34 @@
+package data;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataBaseManagerImpl implements DatabaseManager {
+
+    Connection connection;
+    @Override
+    public void connect() {
+
+    }
+
+    @Override
+    public void createTables() {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public void deleteTables() {
+
+    }
+
+    @Override
+    public void disconnect() {
+
+    }
+}
