965dd79b3eb8bc242c161d16552f010595e78685
Olivia Fountain <123495218+ogf9uhy@users.noreply.github.com>
2023-04-26 19:10:30 -0400

    Making the presentation layer - command line UI

---
+++ src/main/java/data/CommandLineCourseReview.java
@@ -0,0 +1,35 @@
+package data;
+import java.util.*;
+public class CommandLineCourseReview {
+    private Scanner scanner;
+    public static void main(String[] args) {
+
+    }
+
+    public void run() {
+
+    }
+    private void initializeSystem() {
+
+    }
+    private void initializeScanner() {
+        scanner = new Scanner(System.in);
+    }
+    public void login() {
+
+    }
+
+    public void mainMenu() {
+
+    }
+    public void submitReview(Course course) {
+
+    }
+    public void seeReview(Review review) {
+
+    }
+    private String prompt(String s) {
+        System.out.println(s);
+        return scanner.nextLine();
+    }
+}
