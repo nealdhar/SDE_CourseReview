64ea1a36d771d9bc4aa124abc434d2ea2333a1f5
Olivia Fountain <123495218+ogf9uhy@users.noreply.github.com>
2023-04-26 18:04:55 -0400

    Creating interface for DataBaseManager

---
+++ src/main/java/data/DatabaseManager.java
@@ -0,0 +1,21 @@
+package data;
+import java.util.List;
+public interface DatabaseManager {
+    void connect();
+
+    void createTables();
+
+//    void addStudents(List<Student> students);
+
+//    void addCourses(List<Course> courses);
+
+//    void addReviews(List<Review> reviews);
+
+//    Review getReviews(String courseName);
+
+    void clear();
+
+    void deleteTables();
+
+    void disconnect();
+}
