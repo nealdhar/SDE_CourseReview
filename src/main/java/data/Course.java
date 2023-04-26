09b28ecd5bafc96e8c807756a9f73b5bbbabae10
ImFalse <james_jin@rocketmail.com>
2023-04-26 12:34:43 -0400

    Created initial Course and Review classes. Also set up SDK to corretto 17. Starting to work a little more on this project.

---
+++ src/main/java/data/Course.java
@@ -0,0 +1,15 @@
+
+
+public class Course {
+    private int id;
+    private String department;
+    private String catalogNumber;
+
+    public Course(int id, String department, String catalogNumber) {
+        this.id = id;
+        this.department = department;
+        this.catalogNumber = catalogNumber;
+    }
+
+    // Add getters and setters for all attributes
+}
---
+++ src/main/java/data/Review.java
@@ -0,0 +1,18 @@
+
+
+public class Review {
+    private int id;
+    private int studentId;
+    private int courseId;
+    private String message;
+    private int rating;
+
+    public Review(int id, int studentId, int courseId, String message, int rating) {
+        this.id = id;
+        this.studentId = studentId;
+        this.courseId = courseId;
+        this.message = message;
+        this.rating = rating;
+    }
+
+// Add getters and setters for all attributes
