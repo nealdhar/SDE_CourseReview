9fe847758c8ce1a4a67094613c5a2db32ad08004
ImFalse <james_jin@rocketmail.com>
2023-04-26 12:33:31 -0400

    Created initial student class. Completely basic but gives us a starting point.

---
+++ src/main/java/data/Student.java
@@ -0,0 +1,15 @@
+
+
+public class Student {
+    private int id;
+    private String name;
+    private String password;
+
+    public Student(int id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+    // Add getters and setters for all attributes
+}
