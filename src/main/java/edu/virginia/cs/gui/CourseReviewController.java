592b0b16c31a321ed3fea2117c606a9add4b8815
Olivia Fountain <123495218+ogf9uhy@users.noreply.github.com>
2023-05-02 16:16:17 -0400

    Committing gradle files

--- src/main/java/edu/virginia/cs/gui/CourseReviewController.java
+++ src/main/java/edu/virginia/cs/gui/CourseReviewController.java
@@ -1,14 +1,20 @@
 package edu.virginia.cs.gui;
 
+import edu.virginia.cs.data.CourseReviewImplementation;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CourseReviewController implements Initializable {
+
+    CourseReviewImplementation courseReview = new CourseReviewImplementation();
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        // Other things to initialize GUI
+        courseReview.connectDatabase();
+        courseReview.createTables();
     }
+
 
 }
