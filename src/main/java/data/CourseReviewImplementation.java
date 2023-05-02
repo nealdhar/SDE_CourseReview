cd5c0527b14f564217628426545a26485c5875e8
Olivia Fountain <123495218+ogf9uhy@users.noreply.github.com>
2023-05-01 22:10:43 -0400

    Changing CL implementation to a Course Review implementation

---
+++ src/main/java/data/CourseReviewImplementation.java
@@ -0,0 +1,21 @@
+package data;
+import java.util.*;
+
+public class CourseReviewImplementation {
+    public static void main(String[] args) {
+    }
+
+    public void login() {
+
+    }
+
+    public void mainMenu() {
+
+    }
+    public void submitReview(Course course) {
+
+    }
+    public void seeReview(Review review) {
+
+    }
+}
